488be0a2f3d984ae8b91e9ffaa09582206b58bdb
GlobalOffender <99067159@mydavinci.nl>
2021-09-15 10:04:40 +0200

    tafels

---
+++ tafels/tafel.py
@@ -0,0 +1,3 @@
+nummer = input("Geef een nummer op: ")
+for math in range(1,11):
+    print(int(nummer) * int(math))
---
+++ tafels/time.py
@@ -0,0 +1,4 @@
+for am in range(1,13):
+    print(str(am) + " AM")
+for pm in range(1,13):
+    print(str(pm) + " PM")
